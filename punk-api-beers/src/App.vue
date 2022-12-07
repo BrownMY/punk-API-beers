14b5a1cf57bfaebf0016809efe79108f6a486eed
mbrown <tugtuj@gmail.com>
2022-12-07 16:55:31 -0500

    built App code with styling

--- punk-api-beers/src/App.vue
+++ punk-api-beers/src/App.vue
@@ -1,91 +1,109 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import axios from 'axios';
+import ListItem from './components/ListItem.vue';
+
+export default {
+  data() {
+    return {
+      url: 'https://api.punkapi.com/v2/beers?',
+      pageLimit: 20,
+      results: [],
+      searchInput: ''
+    }
+  },
+  mounted() {
+    const response = axios.get(this.url + `per_page=${this.pageLimit}`).then(response => {
+      this.results = response.data
+    })
+  },
+  methods: {
+    //WIP - Not working as of yet - needs to increase page limit by 20 on each click of the button
+    // handlePageLimit() {
+    //   let newPageLimit = this.pageLimit >= 60 ? 80 : this.pageLimit + 20
+    //   this.pageLimit = newPageLimit
+    // },
+
+    // WIP - needs refactoring. A user can filter beers by typing the name, clicking out of the input filed, then clicking inside again. Currently needs reloading to fetch entire list again.
+    handleSearchBar(input) {
+      this.searchInput = input.target.value
+      if (this.searchInput) {
+        const response = axios.get(this.url + `beer_name=${input.target.value}`).then(response => {
+          this.results = response.data
+        })
+      } else {
+        const response = axios.get(this.url + `per_page=${this.pageLimit}`).then(response => {
+          this.results = response.data
+        })
+      }
+    }
+  },
+  components: {
+    ListItem
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <div class="app-container">
+    <div class="header">
+      <h3>Punk API Beers</h3>
+      <input type="text" @click="handleSearchBar" v-model="input" placeholder="Type here to filter by name"
+        class="search-bar">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
-  </header>
+    <ListItem :results="this.results" />
+    <button @click="handlePageLimit">load more beer</button>
+  </div>
 
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.app-container {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h3 {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 24px;
+  font-weight: 600px;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.header {
+  display: flex;
+  flex-direction: row;
+  width: 95vw;
+  margin-top: 2vh;
+  justify-content: space-between;
+  align-items: center;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.search-bar {
+  width: 50vw;
+  height: 20px;
+  border: 2px solid rgb(227, 228, 230);
+  border-radius: 3px;
+  padding: 10px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
+@media only screen and (max-width: 600px) {
+  .header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-direction: column;
+    width: 85vw;
+    margin-top: 2vh;
+    justify-content: center;
+    align-items: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  .search-bar {
+  width: 100vw;
+  height: 20px;
+  border: 2px solid rgb(227, 228, 230);
+  border-radius: 3px;
+  padding: 10px;
+}
 }
 </style>
